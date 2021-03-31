75608edba41e8558ed9c973dc27910f10e2f3004
Raymond Oluoch <rasholuoch@gmail.com>
2021-03-31 21:58:07 +0300

    social login with google

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link, Redirect } from "react-router-dom";
 import { signin, authenticate } from "../auth";
+import SocialLogin from "./SocialLogin";
 
 class Signin extends Component {
   constructor() {
@@ -81,6 +82,10 @@ class Signin extends Component {
     return (
       <div className="container">
         <h2 className="mt-5 mb-5">Sign In</h2>
+
+        <hr />
+        <SocialLogin />
+        <hr />
 
         <div
           className="alert alert-danger"
